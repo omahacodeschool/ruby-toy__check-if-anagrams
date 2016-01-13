df1d6f7ddac3d6a1e0e4c29032d14d93d03f3b45
Ryan Newell <ryanalbertnewell@gmail.com>
2016-01-13 01:37:03 +0000

    Re-Done

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -2,12 +2,14 @@ def anagrams?(str1, str2)
     array_1 = str1.split("")
     array_2 = str2.split("")
     array_3 = array_1.select { |el| !array_2.include?(el) }
-        if array_1.length == array_2.length &&
-            array_3 = array_3.empty?
-            return true
-        else 
-            return false
-        end
+    if array_1.length == array_2.length &&
+    array_3.empty?
+        return true
+    else 
+        return false
+    end
 end
 
-# Hmmmm... Yeah. It's ugly... but it works. And for now, that's my main concern. ;-)
+
+
+#Ok. I think this looks better. Did I get the indentation right this time?
