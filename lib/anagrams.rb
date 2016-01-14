2c548b78e7f6e6ebcbb486bc6f3e5bb2bd82ceb6
amymariewall <amymariewall@gmail.com>
2016-01-14 08:55:11 +0000

    Anagram Exercise Response

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,2 +1,18 @@
 def anagrams?(str1, str2)
+    word_first=str1.split""
+    word_second=str2.split""
+    x=word_first.count
+    y=word_second.count
+    if x != y
+        puts "not anagrams"
+    else
+        z=word_first.map {|i| word_second.include?(i) }
+        if z.include?(false)
+            puts "not anagrams"
+        else 
+            puts "anagrams"
+        end
+    end
 end
+
+anagrams?("chiser", "search")
