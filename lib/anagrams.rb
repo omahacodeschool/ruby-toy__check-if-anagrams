be6d445b3cc417d2481407c8e0088fe33e8c15b2
bethanyhaubert <bethany.haubert@gmail.com>
2016-01-18 01:49:50 +0000

    done

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,5 +1,21 @@
+#def make_string1_alphabetical(str1)
+    #divide the strings into individual characters and store in array
+#    first_word = str1.split("")
+#    return first_word.sort! { |a,b| a.name.downcase <=> b.name.downcase }
+#end
+
+#def make_string2_alphabetical(str2)  
+#    second_word = str2.split("")    
+#    return  second_word.sort! { |a,b| a.name.downcase <=> b.name.downcase }
+#end
+
 def anagrams?(str1, str2)
-    a = str1.split("")
-    b = str2.split("")
-    
+#    a = make_string1_alphabetical(str1)
+#    b = make_string2_alphabetical(str2)
+    #compared alphabetically sorted strings
+    if str1.chars.sort.join == str2.chars.sort.join
+       return true  
+    elsif str1.chars.sort.join != str2.chars.sort.join
+       return false
+    end
 end
