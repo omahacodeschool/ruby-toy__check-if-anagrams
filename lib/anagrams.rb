16c024b35ba2dfcbb6467e9c3d012e09c5a0bfbf
Tyler Fisher <tfisher322@gmail.com>
2016-01-20 01:12:09 +0000

    Anagrams

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,5 +1,4 @@
 def anagrams?(str1, str2)
-   (str1.chars - str2.chars).length.zero? ? true : false
+   return (str1.chars - str2.chars).length.zero? ? true : false
 end
 
-puts anagrams?(str1, str2)
