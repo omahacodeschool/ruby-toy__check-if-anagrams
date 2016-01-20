7ae9d84896aabcd8599a5ad27dec9c646544eba5
Jillian <jwaggoner1016@gmail.com>
2016-01-20 17:15:49 +0000

    Anagrams

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,4 +1,11 @@
 def anagrams?(str1, str2)
-    a=[]
-    b=[]
+    a = []
+    b = []
+        a<<str1.downcase.chars.sort.join
+        b<<str2.downcase.chars.sort.join
+end
+if a == b
+    puts "true"
+else
+    puts "false"
 end
